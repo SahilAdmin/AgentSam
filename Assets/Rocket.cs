42d613d31510c51ad8d94b0582c0d5895988d3bf
DESKTOP-P8S4UUT\SAHIL <smahajan54842@gmail.com>
2020-12-23 02:44:49 +0530

    Till adding thrust.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+
+    private void processInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+            rigidBody.AddRelativeForce(Vector3.up);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            print("Rotating right ");
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            print("Rotating left");
+
+    }
+}
